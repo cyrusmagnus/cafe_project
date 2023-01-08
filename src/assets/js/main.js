11cd53500a7498720705346bf76791aedad24ec7
unknown <kiryasovnikita0@gmail.com>
2023-01-08 18:03:16 +0300

    commit_1

---
+++ src/assets/js/main.js
@@ -0,0 +1,6 @@
+/*  //= components/script2.js - это для подключение дргуих файлов js  */
+
+//= components/burger.js
+// -= components/slider.js
+// -= components/dropdown.js
+// -= components/gallery.js
